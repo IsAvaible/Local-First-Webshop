7116a669e8df055894c9e108a38c25c05b3bc1dd
Simon Conrad <simon.conrad@alumni.fh-aachen.de>
2025-10-15 12:10:42 +0200

    feat(routing): add tanstack router as routing library

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,21 @@
 import { defineConfig } from "vite";
 import path from "path";
 import tailwindcss from "@tailwindcss/vite";
+import { tanstackRouter } from "@tanstack/router-plugin/vite";
 import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     tailwindcss(),
+    tanstackRouter({
+      target: "react",
+      autoCodeSplitting: true,
+      routesDirectory: "./src/routes",
+      generatedRouteTree: "./src/routeTree.gen.ts",
+      routeFileIgnorePrefix: "-",
+      quoteStyle: "double"
+    }),
     react({
       babel: {
         plugins: [["babel-plugin-react-compiler"]]
