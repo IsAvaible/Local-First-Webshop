26bad16616de24290e35797db5643997c2fc2d84
Simon Conrad <simon.conrad@alumni.fh-aachen.de>
2026-01-16 14:33:44 +0100

    feat(meta): add favicon based on lucide package-open

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -40,7 +40,23 @@ export const Route = createRootRoute({
       {
         rel: "stylesheet",
         href: appCss
-      }
+      },
+
+      // --- Favicons ---
+      {
+        rel: "icon",
+        type: "image/png",
+        href: "/favicon/favicon-96x96.png",
+        sizes: "96x96"
+      },
+      { rel: "icon", type: "image/svg+xml", href: "/favicon/favicon.svg" },
+      { rel: "shortcut icon", href: "/favicon.ico" },
+      {
+        rel: "apple-touch-icon",
+        sizes: "180x180",
+        href: "/favicon/apple-touch-icon.png"
+      },
+      { rel: "manifest", href: "/site.webmanifest" }
     ]
   }),
   errorComponent: (props) => {
